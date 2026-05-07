824d38bd51096bbdc6dd6441a9c091adff8e3c55
25021854-afk <25021854@vnu.edu.vn>
2026-05-07 19:02:45 +0700

    abcde

---
+++ Bai9.7/src/main/java/com/example/App.java
@@ -0,0 +1,8 @@
+package com.example;
+
+public class App {
+    private int a_b_c = 10;
+    public static void main(String[] args) {
+        loiSystem.out.println("Hello, Automated Review!");
+    }
+}
